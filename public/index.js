d3438ed66548e854e5c66b9b858c91403b291fa3
raman <raman@figo.africa>
2023-03-21 00:50:19 +0100

    quick-fix

---
+++ public/index.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const matric = document.querySelector("#matric").value;
+  const date = document.querySelector("#date").value;
+  const time = document.querySelector("#time").value;
+
+  const data = { name, email, matric, date, time };
+
+  // validation
+  if (!name || !email || !matric || !date || !time) {
+    alert("Please fill in all fields");
+    return;
+  }
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address");
+    return;
+  }
+  if (!validateDate(date)) {
+    alert("Please enter a valid date in the format DD-MM-YY");
+    return;
+  }
+  if (!validateTime(time)) {
+    alert("Please enter a valid time in the format HH:mm in the 24-hr format");
+    return;
+  }
+  fetch("http://localhost:3000/add", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  }).then(() => {
+    // Redirect to success page
+    window.location.href = "/success.html";
+  });
+
+  form.reset();
+});
+
+function validateEmail(email) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validateDate(date) {
+  const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+  return dateFormat.test(date);
+}
+
+function validateTime(time) {
+  const pattern = /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/;
+  return pattern.test(time);
+}
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cron = require('node-cron');
-const sgMail = require('@sendgrid/mail');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,13 +14,19 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 sgMail.setApiKey(process.env.SG_API_KEY);
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, autoIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  autoIndex: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
@@ -34,74 +41,79 @@ const reminderSchema = new Schema({
   dayReminder: { type: Date, required: true },
 });
 
-const Reminder = mongoose.model('Reminder', reminderSchema);
+const Reminder = mongoose.model("Reminder", reminderSchema);
 
-app.post('/add', async(req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/add", async (req, res) => {
   const { name, email, matric, date, time } = req.body;
-  const examDate = new Date(date + ' ' + time);
+  const examDate = new Date(date + " " + time);
   const minsReminder = new Date(examDate - 15 * 60 * 1000); // 15 minutes before exam
   const hourReminder = new Date(examDate - 60 * 60 * 1000); // 1 hour before exam
   const dayReminder = new Date(examDate - 24 * 60 * 60 * 1000); // 1 day before exam
-  
 
   const confirmationMsg = {
     to: email,
-    from: 'olagunjuismail7@gmail.com',
-    subject: 'Exam reminder registration successful',
+    from: "olagunjuismail7@gmail.com",
+    subject: "Exam reminder registration successful",
     html: `<h1 style="color:green;">Exam Reminder Registration</h1>
     <p>Hello ${name}, you have successfully registered for an exam reminder. Your details are as follows:</p>
     <p><strong>Matric Number:</strong> ${matric}</p>
     <p><strong>Exam Date:</strong> ${examDate.toString()}</p>
     <br><br>
-    <p>Thank you.</p>`
-};
+    <p>Thank you.</p>`,
+  };
 
-try {
+  try {
     await sgMail.send(confirmationMsg);
     res.status(200).send({ message: "Exam reminder set successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error sending confirmation email');
+    res.status(500).send("Error sending confirmation email");
     return;
   }
- 
+
   const newReminder = new Reminder({
     name,
     email,
     examDate,
     minsReminder,
     hourReminder,
-    dayReminder
+    dayReminder,
   });
   newReminder.save().then(() => {
-    console.log('Reminder added!');
+    console.log("Reminder added!");
     scheduleReminder(newReminder);
   });
 });
 
 function scheduleReminder(reminder) {
-  const { name, email, examDate, minsReminder, hourReminder, dayReminder } = reminder;
+  const { name, email, examDate, minsReminder, hourReminder, dayReminder } =
+    reminder;
 
-  
   // Schedule day reminder
   // ...
   // (task1 code remains the same)
-  
+
   const task1 = cron.schedule(
-    `0 ${dayReminder.getMinutes()} ${dayReminder.getHours()} ${dayReminder.getDate()} ${dayReminder.getMonth() + 1} *`,
+    `0 ${dayReminder.getMinutes()} ${dayReminder.getHours()} ${dayReminder.getDate()} ${
+      dayReminder.getMonth() + 1
+    } *`,
     () => {
       const msg = {
         to: email,
-        from: 'olagunjuismail7@gmail.com',
-        subject: 'Reminder: Exam tomorrow',
+        from: "olagunjuismail7@gmail.com",
+        subject: "Reminder: Exam tomorrow",
         html: `<h1 style="color:blue;">Exam Reminder</h1>
               <p>Hello ${name}, this is a friendly reminder that your exam is tomorrow ${examDate.toString()}.</p>
               <br><br>
               <p>Prepare well and be punctual to the venue</p>
               <br><br>
-              <p>Best of luck!</p>`
+              <p>Best of luck!</p>`,
       };
-      sgMail.send(msg).catch(error => {
+      sgMail.send(msg).catch((error) => {
         console.error(error);
       });
     },
@@ -111,24 +123,25 @@ function scheduleReminder(reminder) {
   );
   task1.start();
 
-
   // Schedule hour reminder
   // ...
   // (task2 code remains the same)
 
   const task2 = cron.schedule(
-    `0 ${hourReminder.getMinutes()} ${hourReminder.getHours()} ${hourReminder.getDate()} ${hourReminder.getMonth() + 1} *`,
+    `0 ${hourReminder.getMinutes()} ${hourReminder.getHours()} ${hourReminder.getDate()} ${
+      hourReminder.getMonth() + 1
+    } *`,
     () => {
       const msg = {
         to: email,
-        from: 'olagunjuismail7@gmail.com',
-        subject: 'Reminder: Exam in an hour',
+        from: "olagunjuismail7@gmail.com",
+        subject: "Reminder: Exam in an hour",
         html: `<h1 style="color:blueviolet;">Exam Reminder</h1>
         <p>Hello ${name}, this is a friendly reminder that your exam is in an hour at ${examDate}.</p>
         <br><br>
-        <p>Best of luck!</p>`
+        <p>Best of luck!</p>`,
       };
-      sgMail.send(msg).catch(error => {
+      sgMail.send(msg).catch((error) => {
         console.error(error);
       });
     },
@@ -136,25 +149,26 @@ function scheduleReminder(reminder) {
       scheduled: false,
     }
   );
- task2.start();
-
+  task2.start();
 
   // Schedule 15 minutes reminder
   // ...
   // (task3 code remains the same)
   const task3 = cron.schedule(
-    `0 ${minsReminder.getMinutes()} ${minsReminder.getHours()} ${minsReminder.getDate()} ${minsReminder.getMonth() + 1} *`,
+    `0 ${minsReminder.getMinutes()} ${minsReminder.getHours()} ${minsReminder.getDate()} ${
+      minsReminder.getMonth() + 1
+    } *`,
     () => {
       const msg = {
         to: email,
-        from: 'olagunjuismail7@gmail.com',
-        subject: 'Reminder: Exam in 15 minutes',
+        from: "olagunjuismail7@gmail.com",
+        subject: "Reminder: Exam in 15 minutes",
         html: `<h1 style="color:red;">Exam Reminder</h1>
         <p>Hello ${name}, this is a friendly reminder that your exam starts in the next 15 minutes. Gather all your writing materials and head to your venue right now</p>
         <br><br>
-        <p>Best of luck ${name}!</p>`
+        <p>Best of luck ${name}!</p>`,
       };
-      sgMail.send(msg).catch(error => {
+      sgMail.send(msg).catch((error) => {
         console.error(error);
       });
     },
@@ -174,7 +188,4 @@ app.listen(port, async () => {
   reminders.forEach((reminder) => {
     scheduleReminder(reminder);
   });
-
 });
-
-
